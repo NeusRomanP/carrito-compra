f2930428c5991e33cad249da3646213856b3896e
Neus Roman <neusroman@hotmail.com>
2024-11-16 18:36:19 +0100

    fetch product from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { ProductForm } from './ProductForm.js'
+import { ProductForm } from './ProductForm'
+import { useState, useEffect } from 'react'
 import './App.css'
+import Product from './interfaces/Product';
 
 function App() {
+  const [products, setProducts] = useState<any>([]);
+
+  const addProduct = (product: Product) => {
+    setProducts((prevProducts: Product[]) => [...prevProducts, product]);
+  }
+
+  useEffect(() => {
+    console.log(products);
+  }, [products]);
 
   return (
     <>
-      <ProductForm />
+      <ProductForm addProduct={addProduct}/>
     </>
   )
 }
--- src/ProductForm.tsx
+++ src/ProductForm.tsx
@@ -1,12 +1,47 @@
-import './ProductForm.css'
-export function ProductForm() {
+import { useState } from 'react';
+import './ProductForm.css';
+import Product from './interfaces/Product';
+
+type Props = {
+  addProduct: (product: Product) => void,
+}
+
+export function ProductForm({ addProduct }: Props) {
+  const [amount, setAmount] = useState('');
+  const [productId, setProductId] = useState('');
+
+  const handleChangeAmount = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setAmount(value);
+  };
+
+  const handleChangeProductId = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setProductId(value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const res = await fetch(`https://fakestoreapi.com/products/${productId}`);
+    const data = await res.json();
+
+    addProduct(data);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <p>Agrega los productos al carro de compra</p>
       <div className='form-items'>
         <div className='inputs'>
-          <input type="number" placeholder="Cantidad" id='amount' />
-          <input type="text" placeholder="ID del Producto" id='productId' />
+          <input type="number"
+                 placeholder="Cantidad"
+                 id='amount'
+                 value={amount}
+                 onChange={handleChangeAmount} />
+          <input type="text"
+                 placeholder="ID del Producto"
+                 id='productId'
+                 value={productId}
+                 onChange={handleChangeProductId} />
         </div>
         <button type="submit">Agregar</button>
       </div>
